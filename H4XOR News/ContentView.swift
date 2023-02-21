57edef98bda7d187e2969b62be8a11979622cfa0
Chima-Anason <tblynxx@gmail.com>
2023-02-21 23:13:59 +0800

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by mac on 21/02/2023.
+//  Copyright © 2023 Chima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
