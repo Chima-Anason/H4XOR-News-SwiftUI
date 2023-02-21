15fd6122bce7c3a772f085d4f56ee718f1a3f9a6
Chima-Anason <tblynxx@gmail.com>
2023-02-21 23:38:21 +0800

    Use of List

--- H4XOR News/ContentView.swift
+++ H4XOR News/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List(posts){ post in
+                Text(post.title)
+            }
+        .navigationBarTitle("H4XOR News ")
+        }
+        
+        
     }
 }
 
@@ -19,3 +26,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "hello"),
+    Post(id: "2", title: "hey"),
+    Post(id: "3", title: "cool")
+]
